ddaf81b0261a73e5ad3e986696805474e0888752
jhuncho <jhuncho@DESKTOP-E4OTMPB>
2018-10-26 15:36:06 +0900

    aa

---
+++ FreeHtmlToPdf/src/FreeHtmlToPdf.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+
+import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+public class FreeHtmlToPdf {
+
+	
+	public static void main(String[] args) throws Exception {
+	    File file = new File("asd.html");
+	    InputStream is = null;
+	    try
+	    {
+	      is = new FileInputStream(file);
+	    }
+	    catch (FileNotFoundException e)
+	    {
+	      e.printStackTrace();
+	    }
+	    StringWriter sw = new StringWriter();
+	    StringBuffer sb = sw.getBuffer();
+	    Tidy tidy = new Tidy();
+	    tidy.setQuiet(false);
+	    tidy.setShowWarnings(true);
+	    tidy.setInputEncoding("UTF-8");
+	    tidy.setOutputEncoding("UTF-8");
+	    
+	    tidy.setForceOutput(true);
+	    tidy.parse(is, sw);
+	    
+	    String outputFile = "asd.pdf";
+	    OutputStream os = new FileOutputStream(outputFile);
+	    
+	    ITextRenderer renderer = new ITextRenderer();
+	    renderer.setDocumentFromString(sb.toString());
+	    renderer.getFontResolver().addFont("notokr-regular.TTF", "Identity-H", false);
+	    renderer.layout();
+	    renderer.createPDF(os, false);
+	    
+	    renderer.setDocumentFromString(sb.toString());
+	    renderer.getFontResolver().addFont("notokr-regular.TTF", "Identity-H", false);
+	    renderer.layout();
+	    renderer.writeNextDocument();
+	    
+	    renderer.finishPDF();
+	    os.close();
+	}
+}
